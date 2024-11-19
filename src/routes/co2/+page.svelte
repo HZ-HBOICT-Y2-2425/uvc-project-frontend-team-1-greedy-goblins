34a9767ec779671c31851a39bd4162e16c3313e1
Brandon 'Okidokie98' de Baat <baat0004@hz.nl>
2024-11-19 11:10:01 +0100

    De frontend route /co2 toegevoegd

---
+++ src/routes/co2/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	// data is a prop passed from the server
+	export let data;
+</script>
+
+<section class="container">
+	<h2 class="text-center text-2xl my-6">CO2 Stats</h2>
+  <p>Hier komen de stats over Co2.</p>
+</section>

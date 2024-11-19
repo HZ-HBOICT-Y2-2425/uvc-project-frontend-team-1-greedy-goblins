1001d77bc92ff711114f0ba0370fa83c13cfeedc
Kylian <held0005@hz.nl>
2024-11-19 12:33:30 +0100

    added some basic pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,5 @@
 
 <h1 class="bg-lime-400">Welcome to SvelteKit</h1>
 <p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
+    This is the dashboard
 </p>

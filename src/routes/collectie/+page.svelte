f01c22fcf97b380fc03f838c7d93c09c1eb47750
Kylian <held0005@hz.nl>
2024-11-20 11:05:48 +0100

    made the collections different from eachother

--- src/routes/collectie/+page.svelte
+++ src/routes/collectie/+page.svelte
@@ -1,42 +1,38 @@
 <script>
     import "../../app.css";
 
-    let kaart1 = false;
     let kaart2 = false;
     let kaart3 = false;
     let kaart4 = false;
-    let kaarten = [kaart1, kaart2, kaart3, kaart4];
+    let kaarten = [[false, false, false, false], [false]];
 
-    function eersteKaart() {
-        kaart1 = kaart1 === false;
-    }
-
-    function krijgKaart(number) {
-        kaarten[number] = true;
+    function krijgKaart(groep, number) {
+        kaarten[groep][number] = true
         console.log({number});
-        console.log(kaarten[number]);
-        console.log(kaart1);
+        console.log(kaarten[groep[number]]);
     }
 
-    function kwartet() {
-        kaart1 = false;
-        kaart2 = false;
-        kaart3 = false;
-        kaart4 = false;
-        for (let i = 0; i < kaarten.length; i++) {
-            kaarten[i] = false;
+    function kwartet(groep) {
+        for (let i = 0; i < kaarten[groep].length; i++) {
+            kaarten[groep][i] = false;
         }
+        console.log(kaarten);
+    }
+
+    function zieCollectie() {
+        console.log({kaarten});
     }
 </script>
 
 <h1>Mijn collectie</h1>
+<h2>Groente</h2>
 <div class="qwartet">
-    {#if kaarten[0] === false}
+    {#if kaarten[0][0] === false}
         <p class="kaart">kaart 1</p>
     {:else }
         <p class="kaart collected">kaart 1</p>
     {/if}
-    {#if kaart2 === false}
+    {#if kaarten[0][1] === false}
         <p class="kaart">kaart 2</p>
     {:else }
         <p class="kaart collected">kaart 2</p>
@@ -52,10 +48,21 @@
         <p class="kaart collected">kaart 4</p>
     {/if}
 </div>
+<h2>Fruit</h2>
+<div class="qwartet">
+    {#if kaarten[1][0] === false}
+        <p class="kaart">kaart 5</p>
+    {:else}
+        <p class="kaart collected">kaart 5</p>
+    {/if}
+</div>
 
-<button on:click={() =>eersteKaart()}>kaart 1 {kaart1}</button>
-<p><button on:click={() =>krijgKaart(0)}>kaart 1</button></p>
-<p><button on:click={() =>kwartet()}>reset</button></p>
+<br>
+<p><button on:click={() =>krijgKaart(0, 1)}>kaart 1</button></p>
+<br>
+<p><button on:click={() =>krijgKaart(1, 0)}>kaart 5</button></p>
+<p><button on:click={() =>kwartet(0)}>reset</button></p>
+<p><button on:click={() => zieCollectie()}>collectie</button></p>
 
 <style>
     .qwartet {

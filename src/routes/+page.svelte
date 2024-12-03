ce52c0eae3c71c47454ea73ea25b654f11a71ed6
Brandon 'Okidokie98' de Baat <baat0004@hz.nl>
2024-12-03 13:29:22 +0100

    MarketBox (hoofdpagina) component gemaakt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import "../app.css";
   import { onMount } from "svelte";
+  import MarketBox from "../lib/components/MarketBox.svelte";
 
   /**
    * @type {any[]}
@@ -59,18 +60,6 @@
   Uw favoriete plaatsen
 </div>
 
-<main class="grid grid-cols-1 sm:grid-cols-3 gap-4 px-4 py-4 mb-24">
-  {#each filteredLocations as marketInfo}
-    <button on:click={() => window.location.href=`/location/${marketInfo.marketID}`}
-      class="bg-green-500 text-white rounded-lg shadow-lg p-6 flex flex-col justify-center items-center hover:bg-green-600"
-      aria-label={marketInfo.marketName}
-    >
-      <img
-        src="farmer.png"
-        alt={marketInfo.marketName}
-        class="h-48 w-48 mb-4 object-cover"
-      />
-      <h2 class="text-lg font-bold">{marketInfo.marketName}</h2>
-    </button>
-  {/each}
+<main>
+    <MarketBox {filteredLocations} />
 </main>

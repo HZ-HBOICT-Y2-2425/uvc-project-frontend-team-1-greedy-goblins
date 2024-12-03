1bcfbd11bd88e67fa65eb7387b2cbcd23f1ebc07
Brandon 'Okidokie98' de Baat <baat0004@hz.nl>
2024-12-03 10:19:45 +0100

    Updated the request call

--- src/routes/location/[id]/+page.svelte
+++ src/routes/location/[id]/+page.svelte
@@ -10,7 +10,7 @@
 
   onMount(async () => {
     const response = await fetch(
-      `http://localhost:3010/microserviceMarket/MarketInfo/10${id}`
+      `http://localhost:3010/microserviceMarket/MarketInfo/${id}`
     );
     locationData = await response.json();
   });

d9f6351750caa5344599aa72efb1efb57b6c57f2
FroyoFrozen <will0145@hz.nl>
2024-12-10 11:29:48 +0100

    laatste paar aanpassingen calculator

--- src/routes/co2/+page.svelte
+++ src/routes/co2/+page.svelte
@@ -8,9 +8,10 @@
   let userLatitude = 0;
   let userLongitude = 0;
   let distance = 0;
-  let productEmission = 0;
-  let transportEmission = 0.25;
-  let totalEmission = 0;
+  let productFactor = 0;
+  let userEmission = 0;
+  let storeEmission = 0;
+  let totalEmissionSaved = 0;
   let loadingDistance = true;
 
   // Huidige locatie ophalen en schoolafstand berekenen
@@ -18,9 +19,6 @@
     .then(async (coords) => {
       userLatitude = coords.latitude;
       userLongitude = coords.longitude;
-      console.log(
-        `Huidige locatie: Latitude: ${userLatitude}, Longitude: ${userLongitude}`
-      );
 
       // Loop door de orders in de store
       for (const order of $orderStore.orderStore) {
@@ -33,23 +31,22 @@
         );
 
         distance = routeInfo.distanceKm;
-        console.log(`Afstand tussen huidige locatie en boer: ${distance} km`);
 
-        // Bereken de emissie voor de producten in het order
-        productEmission = productEmissionCalculator(order.productsOrdered);
+        productFactor = productEmissionCalculator(order.productsOrdered);
 
-        // Bereken de totale CO2 uitstoot
-        totalEmission =
-          calculateCo2Emissions(productEmission, distance, transportEmission) ??
-          0;
+        storeEmission = productFactor * 165;
 
-        console.log(`Totale Co2 verspilling: ${totalEmission} kg`);
+        userEmission =
+          calculateCo2Emissions(productFactor, distance, 0.08) ?? 0;
+
+        totalEmissionSaved = Math.floor(storeEmission - userEmission);
+        console.log("Total Emission Saved: ", totalEmissionSaved);
 
         // Update de store met de nieuwe totalEmission voor dit order
         orderStore.update((store) => {
           const updatedOrderStore = store.orderStore.map((o) => {
             if (o.id === order.id) {
-              return { ...o, totalEmission }; // Werk de emissie bij
+              return { ...o, totalEmissionSaved };
             }
             return o;
           });
@@ -74,7 +71,8 @@
       </p>
       <p class="text-gray-600">
         {order.productsOrdered.length} producten - Total Emission:
-        <span class="font-bold text-red-500">{order.totalEmission} kg</span>
+        <span class="font-bold text-red-500">{order.totalEmissionSaved} kg</span
+        >
       </p>
       <ul class="space-y-2">
         {#each order.productsOrdered as product}

295e90e7651532047c8cfd0fc9dd5bf6838430d3
DRPHz <144113616+DRPHz@users.noreply.github.com>
2024-12-10 14:03:43 +0100

    Style aanpassingen Location page

--- src/routes/location/[id]/+page.svelte
+++ src/routes/location/[id]/+page.svelte
@@ -22,7 +22,7 @@
     {locationData.marketAdress}
   </div>
 
-  <main class="grid grid-cols-1 sm:grid-cols-3 gap-4 px-4 py-4 mb-24">
+  <main class="grid grid-cols-1 sm:grid-cols-3 gap-4 px-4 py-4 my-2">
     <div
       class="bg-green-500 text-white rounded-lg shadow-lg p-6 flex flex-col justify-center items-center hover:bg-green-600"
     >
@@ -31,7 +31,7 @@
     </div>
 
     <div
-      class="bg-cyan-500 text-black rounded-lg shadow-lg p-6 flex flex-col justify-center items-center hover:bg-green-600"
+      class="bg-cyan-500 text-black rounded-lg shadow-lg p-6 flex flex-col justify-center items-center hover:bg-cyan-600"
     >
       <p class="font-bold">Omschrijving:</p>
       <p>{locationData.marketDesc}</p>

23f51fc711b00e8ae91ef92ae87236c48e2944e1
Kylian <held0005@hz.nl>
2024-12-11 13:34:41 +0100

    kaarten toegevoegd

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -3,14 +3,13 @@
 
   export const title = "Collection";
 
-  let collecties = [{name: "groente", qwartet: false}, {name: "zuivel", qwartet:  false}, {name: "fruit", qwartet: false}, {name: "vlees", qwartet: false}, {name: "placeholder", qwartet: false}];
+  let collecties = [{name: "groente", qwartet: false, uncollected: "/kaarten/uncollected/Groente_uncollected.png"}, {name: "zuivel", qwartet:  false, uncollected: "/kaarten/uncollected/Zuivel_uncollected.png"}, {name: "fruit", qwartet: false, uncollected: "/kaarten/uncollected/Fruit_uncollected.png"}, {name: "vlees", qwartet: false, uncollected: "/kaarten/uncollected/Vlees_uncollected.png"}, {name: "vis", qwartet: false, uncollected: "/kaarten/uncollected/Vis_uncollected.png"}];
   let kaarten = [
     [{name: "wortels", collected: false}, {name: "aardappel", collected: false}, {name: "radijs", collected: false}, {name: "rode biet", collected: false}],
     [{name: "verse melk", collected: false}, {name: "kaas", collected: false}, {name: "yogurt", collected: false}, {name: "boter", collected: false}],
     [{name: "aardbijen", collected: false}, {name: "appels", collected: false}, {name: "peren", collected: false}, {name: "perzikken", collected: false}],
     [{name: "kip", collected: false}, {name: "koe", collected: false}, {name: "varken", collected: false}, {name: "schaap", collected: false}],
-    [{name: "", collected: false}, {name: "", collected: false}, {name: "", collected: false}, {name: "", collected: false}]];
-  //let objecten = [{name: "wortels", collected: false}];
+    [{name: "zalm", collected: false}, {name: "haring", collected: false}, {name: "kabbeljouw", collected: false}, {name: "tonijn", collected: false}]];
 
   let packOpeningKaart1 = "";
   let packOpeningKaart2 = "";
@@ -80,10 +79,16 @@
         }
       }
        */
-      console.log(randomCollectie);
       collectieNummers[i] = randomCollectie;
-    //console.log(collectieNummers);
     }
+    /**
+     * let randomKaarten = []
+     *     for (let i = 0; i < collectieNummers.length; i++) {
+     *       let randomKaart = randomNumberGenerator(kaarten[collectieNummers[i]].length)
+     *       randomKaarten[i] = randomKaart;
+     *       krijgKaart(collectieNummers[i], randomKaarten[i]);
+     *     }
+     */
     let randomKaart1 = randomNumberGenerator(kaarten[collectieNummers[0]].length);
     let randomKaart2 = randomNumberGenerator(kaarten[collectieNummers[1]].length);
     let randomKaart3 = randomNumberGenerator(kaarten[collectieNummers[2]].length);
@@ -92,25 +97,23 @@
     packOpeningKaart3 = "kaart 3 = " + kaarten[collectieNummers[2]][randomKaart3].name + ", " + collectieNummers[2];
 
 
-
     krijgKaart(collectieNummers[0], randomKaart1);
     krijgKaart(collectieNummers[1], randomKaart2);
     krijgKaart(collectieNummers[2], randomKaart3);
   }
 </script>
 
-<div class="p-6">
-  <h1 class="text-2xl font-bold">Collection</h1>
-  <p>Hier komt je collectieoverzicht.</p>
-</div>
-
-<h1>Mijn collectie</h1>
+<h1 class="titel">Mijn collectie</h1>
 
 {#each {length: collecties.length} as collectie, i}
-  <h2>{collecties[i].name}</h2>
+  <h2 class="collectie">{collecties[i].name}</h2>
   <div class="qwartet">
     {#each {length: kaarten[i].length} as kaart, j}
-      <p class={kaarten[i][j].collected ? 'collected' : 'kaart'}>|{kaarten[i][j].name}|</p>
+      {#if kaarten[i][j].collected}
+        <p class="collected">|{kaarten[i][j].name}|</p>
+      {:else }
+        <img class="kaart" alt="kaart nog niet gekregen" src="{collecties[i].uncollected}">
+      {/if}
     {/each}
     {#if collecties[i].qwartet}
       <button on:click={() => kwartet(i)} class="kaart" style="background-color: yellow">kwartet</button>
@@ -118,18 +121,32 @@
   </div>
 {/each}
 
-<p><button style="padding: 2px 10px 2px 10px; color: white" class="bg-green-500 hover:bg-green-600 rounded-lg" on:click={() =>packOpening(3)}>Pack Openen</button></p>
+<p style="text-align: center"><button style="padding: 2px 10px 2px 10px; color: white" class="bg-green-500 hover:bg-green-600 rounded-lg" on:click={() =>packOpening(3)}>Pack Openen</button></p>
 <p>{packOpeningKaart1}</p>
 <p>{packOpeningKaart2}</p>
 <p>{packOpeningKaart3}</p>
+<br>
+<br>
+<br>
+<br>
 
 <style>
+  .titel {
+    text-align: center;
+    font-size: 50px;
+  }
+
+  .collectie {
+    font-size: 30px;
+  }
+
   .qwartet {
     display: flex;
   }
 
   .kaart {
     margin-left: 5px;
+    width: 250px;
   }
 
   .collected {

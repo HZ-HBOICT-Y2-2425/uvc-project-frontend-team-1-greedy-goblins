792f9fbfe9bad0d36a4bf08280297c97fe70138f
DRPHz <144113616+DRPHz@users.noreply.github.com>
2024-12-17 12:42:17 +0100

    styling plaatjes in marketvoerview

--- src/routes/location/[id]/+page.svelte
+++ src/routes/location/[id]/+page.svelte
@@ -17,25 +17,43 @@
   });
 </script>
 
+<svelte:head>
+  {#if locationData}
+    <title>LGHub-{locationData.marketName}</title>
+  {:else}
+    <title>Loading...</title>
+  {/if}
+  <meta name="description" content="LGHub MarketOverview" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</svelte:head>
+
 {#if locationData}
-  <div class="bg-yellow-500 text-center py-2 font-bold text-lg">
-    {locationData.marketAdress}
+  <div
+    class="relative flex flex-col justify-center items-center border-green-500"
+  >
+    <img
+      src={locationData.marketImage}
+      alt={locationData.marketName}
+      class=" md:h-1/4 md:w-1/4"
+    />
+    <div
+      class="absolute bottom-0 w-full bg-green-800 bg-opacity-90 text-white text-center py-2"
+    >
+      <h1 class="text-base font-bold px-2">
+        {locationData.marketName}
+      </h1>
+      <hr />
+      <h2 class="text-base font-semibold px-2">
+        {locationData.marketAdress}
+      </h2>
+      <hr />
+      <h3 class="text-base font-semibold px-2">
+        <p>21 Km verderop</p>
+      </h3>
+    </div>
   </div>
 
-  <div
-  class=" flex flex-col justify-center items-center"
->
-  <img src="{locationData.marketImage}" alt="{locationData.marketName}" class="mb-6 object-cover h-1/4 w-1/4" />
-</div>
-
-  <main class="grid grid-cols-1 sm:grid-cols-3 gap-4 px-4 py-4 my-2">
-    <div
-      class="bg-green-500 text-white rounded-lg shadow-lg p-6 flex flex-col justify-center items-center hover:bg-green-600"
-    >
-      <p class="font-bold">Naam:</p>
-      <p>{locationData.marketName}</p>
-    </div>
-
+  <main class="grid grid-cols-1 sm:grid-cols-3 gap-4 px-4 py-4">
     <div
       class="bg-cyan-500 text-black rounded-lg shadow-lg p-6 flex flex-col justify-center items-center hover:bg-cyan-600"
     >
